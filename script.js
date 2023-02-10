4f2949a6039c547269969dafee744c80a2529707
Arthur Elleres <arthur.elleres@itec.ufpa.br>
2023-02-10 12:25:26 -0300

    Add clear and delete button

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 var string = "";
 
+const clear = document.querySelectorAll(".clear")
 const screen = document.querySelector(".screen")
-const buttons = document.querySelectorAll(".button")
-buttons.forEach(button => {
+const buttonsNum = document.querySelectorAll(".buttonNum")
+buttonsNum.forEach(button => {
     button.addEventListener("click", () => {
-        string += button.id.toString()
+        if (button.id != "dot") {
+            string += button.id.toString()
+        } else {
+            string += "."
+        }
         if (string.length < 25) {
             screen.textContent = `${string}`
         } else {
             alert("max number size")
         }
 
+    })
+})
+
+clear.forEach(button => {
+    button.addEventListener("click", () => {
+        if (button.id == "clear") {
+            string = ""
+            screen.textContent = `${string}`
+        } else {
+            string = string.slice(0, string.length - 1)
+            screen.textContent = `${string}`
+        }
     })
 })
 
